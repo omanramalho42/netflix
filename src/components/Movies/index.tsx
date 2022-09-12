b9fe15287126124b8d33f1f328a78be9083d40ba
omanramalho <omanapple42@hotmail.com>
2022-09-12 18:13:49 -0300

    fix:fazendo requisicao dos filmes

--- src/components/Movies/index.tsx
+++ src/components/Movies/index.tsx
@@ -1,28 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 
 const Movies: React.FC = () => {
-
-  const [movies, setMovies] = useState();
-  const fetchMovies = async () => {
+  const [movies, setMovies] = useState([]);
+  const fetchMovies = useCallback( async () => {
     try {
-      await axios.get('http://localhost:3000/api/movies', { headers: { token: `Bearer ` }})
-        .then((res: any) => setMovies(res))
+      await axios.get('http://localhost:3000/api/movies', 
+        { headers: { 
+          token: 
+            `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMWU1ZmFkZGRkMGQ4OWQ3YmE4YjEyYSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2Mjk0NzIyMiwiZXhwIjoxNjYzMzc5MjIyfQ.Cc-HZ-myGGvAaIk9SCY2gfvgti5T5QHG9nLkmR7_Gf8` 
+          }
+        }).then((res: any) => setMovies(res.data))
         .catch((error) => console.log(error)
       );
   
-      console.log(movies,' movies');
     } catch (error) {
       throw error;
     }
-  }
+  },[movies]);
+
   useEffect(() => {
-    fetchMovies();
-  },[])
+    (async () => (
+      await fetchMovies()
+    ))()
+    
+    console.log(movies,' movies');
+  },[]);
   
   return (
-    <div>
-      <h1>Movies</h1>
+    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+      {movies?.map((i: any, index) => (
+        <div
+          key={index} 
+          style={{ padding: 30 }}>
+          { i.title }
+        </div>
+      ))}
     </div>
   )
 }
